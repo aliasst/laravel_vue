f2c3c1105ba7e6bc47a8263843d03275e871c4f5
aliasst <ahtubafishing@yandex.ru>
2023-12-04 17:06:14 +0300

    lesson 7

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Post Component</div>
+                    <div class="card-body">
+                        I'm an post component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
