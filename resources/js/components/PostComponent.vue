0ae76d1b93d72c709ba9e51f9d77a6596c461e0e
aliasst <ahtubafishing@yandex.ru>
2023-12-04 18:40:48 +0300

    lesson 13

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -5,9 +5,26 @@
                 <div class="card">
                     <div class="card-header">Post Component</div>
                     <div class="card-body">
-                        I'm an post component.
-                        <div>Имя: {{name}}</div>
-                        <button @click="sayHello">Hello</button>
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Age</th>
+                                <th scope="col">Job</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr v-for="person in personsMore" >
+                                <th scope="row">{{person.id}}</th>
+                                <td>{{person.name}}</td>
+                                <td>{{person.age}}</td>
+                                <td>{{person.job}}</td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+
                     </div>
 
                     <SinglePostComponent></SinglePostComponent>
@@ -25,13 +42,58 @@ export default {
     data() {
         return {
             name: "Vasya",
+            persons: [
+                {
+                    id: 1,
+                    name: "Vasya",
+                    age: 20,
+                    job: "coach"
+                },
+                {
+                    id: 2,
+                    name: "Misha",
+                    age: 39,
+                    job: "proger"
+                },
+                {
+                    id: 3,
+                    name: "Oleg",
+                    age: 40,
+                    job: "rab"
+                },
+                {
+                    id: 4,
+                    name: "Timon",
+                    age: 20,
+                    job: "secret"
+                },
+                {
+                    id: 5,
+                    name: "Ivan",
+                    age: 50,
+                    job: "driver"
+                },
+
+            ]
         }
     },
     methods: {
         sayHello() {
-            alert(this.name);
+            //alert(this.name);
+            alert(this.test)
         },
     },
+    computed: {
+        test() {
+            return this.name + ' работает в рюмочной';
+        },
+        personsMore(){
+            return this.persons.filter(function (person){
+                return person.age > 20;
+            });
+        }
+    },
+
     components: {SinglePostComponent}
 }
 </script>
