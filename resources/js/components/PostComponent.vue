762d72a0b59396539a091548acd0cb408757c27c
aliasst <ahtubafishing@yandex.ru>
2023-12-04 17:37:53 +0300

    lesson 10

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -6,7 +6,11 @@
                     <div class="card-header">Post Component</div>
                     <div class="card-body">
                         I'm an post component.
+                        <div>Имя: {{name}}</div>
+                        <button @click="sayHello">Hello</button>
                     </div>
+
+                    <SinglePostComponent></SinglePostComponent>
                 </div>
             </div>
         </div>
@@ -14,8 +18,21 @@
 </template>
 
 <script>
+import SinglePostComponent from "@/components/SinglePostComponent.vue";
+
 export default {
-    name: "PostComponent"
+    name: "PostComponent",
+    data() {
+        return {
+            name: "Vasya",
+        }
+    },
+    methods: {
+        sayHello() {
+            alert(this.name);
+        },
+    },
+    components: {SinglePostComponent}
 }
 </script>
 
---
+++ resources/js/components/SinglePostComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Single Post Component</div>
+                    <div class="card-body">
+                        I'm an single post component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SinglePostComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
